0f2b42ae6bfc957f60dbec367558dd83012aeded
raywhelan01 <raywhelan01@gmail.com>
2020-03-20 15:19:48 -0700

    Sample map update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,9 +2,6 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
       40.7, -94.5
